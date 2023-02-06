edb3d6b258af15e5b579752483b006551ab03b06
maucruzbr <cruzm.br@outlook.com>
2023-02-05 22:24:47 -0300

    Corrigido caminho do bucket do state do terraform

--- infraestructure/provider.tf
+++ infraestructure/provider.tf
@@ -5,8 +5,8 @@ provider "aws" {
 # Centralizar o arquivo de controle de estado do terraform
 terraform {
   backend "s3"{
-    bucket = "terraform-state-edc-usecase"
-    key = "state/igti/edc/mod1-usecase/terraform.tfstate"
+    bucket = "terraform-state-edc-desafio"
+    key = "state/igti/edc/terraform.tfstate"
     region = "sa-east-1"
   }
 }
